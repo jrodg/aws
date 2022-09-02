302f476b817d794071b236f42f50b624a75f4be3
jrodg <jrod.201@hotmail.com>
2022-09-01 22:55:37 -0400

    init comit

--- lib/aws-stack.ts
+++ lib/aws-stack.ts
@@ -1,11 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {Bucket} from 'aws-cdk-lib/aws-s3'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    new Bucket(this, 'ibucket')
     // The code that defines your stack goes here
 
     // example resource
